bdde07d156df40afae28ec1e7f7a6f2e9dac6668
lechangalex <le.chang@mail.utoronto.ca>
2021-08-04 00:25:15 +0000

    refactored segmentation and classification encoders to allow users to plug in their backbones

--- models/segmentation.py
+++ models/segmentation.py
@@ -1,71 +1,71 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from models.custom_encoder import SegmentationEncoder
+
+# def get_segmentation_model(backbone, feature_indices, feature_channels):
+#     """Creates a UNet from a pretrained backbone
+
+#     Args:
+#         backbone (torch.nn.Module): Pre-trained backbone in the form of "Sequential"
+#         feature_indices (list(int)): Indices in the Sequential backbone from which to extract intermediate features
+#         feature_channels ([type]): Number of channels per feature extracted
+
+#     Returns:
+#         [type]: [description]
+#     """
+#     model = SegmentationEncoder(backbone, feature_indices, feature_channels, diff=True)
+#     unet = UNet(model, feature_channels, 1, bilinear=True, concat_mult=1, dropout_rate=0.3)
+#     # unet = UNetSmall(model, feature_channels, 1, bilinear=True, concat_mult=1)
+#     return unet
 
 
-def get_segmentation_model(backbone, feature_indices, feature_channels):
-    """Creates a UNet from a pretrained backbone
+# class SegmentationEncoder(torch.nn.Module):
+#     def __init__(self, backbone, feature_indices, feature_channels, diff=False):
+#         super().__init__()
+#         self.feature_indices = list(sorted(feature_indices))
 
-    Args:
-        backbone (torch.nn.Module): Pre-trained backbone in the form of "Sequential"
-        feature_indices (list(int)): Indices in the Sequential backbone from which to extract intermediate features
-        feature_channels ([type]): Number of channels per feature extracted
+#         # A number of channels for each encoder feature tensor, list of integers
+#         self._out_channels = feature_channels  # [3, 16, 64, 128, 256, 512]
 
-    Returns:
-        [type]: [description]
-    """
-    model = SegmentationEncoder(backbone, feature_indices, feature_channels, diff=True)
-    unet = UNet(model, feature_channels, 1, bilinear=True, concat_mult=1, dropout_rate=0.3)
-    # unet = UNetSmall(model, feature_channels, 1, bilinear=True, concat_mult=1)
-    return unet
+#         # Default number of input channels in first Conv2d layer for encoder (usually 3)
+#         self._in_channels = 3
 
+#         # Define encoder modules below
+#         self.encoder = backbone
 
-class SegmentationEncoder(torch.nn.Module):
-    def __init__(self, backbone, feature_indices, feature_channels, diff=False):
-        super().__init__()
-        self.feature_indices = list(sorted(feature_indices))
+#         self.diff = diff
 
-        # A number of channels for each encoder feature tensor, list of integers
-        self._out_channels = feature_channels  # [3, 16, 64, 128, 256, 512]
+#     def forward(self, x1, x2):
+#         """Produce list of features of different spatial resolutions, each feature is a 4D torch.tensor of
+#         shape NCHW (features should be sorted in descending order according to spatial resolution, starting
+#         with resolution same as input `x` tensor).
 
-        # Default number of input channels in first Conv2d layer for encoder (usually 3)
-        self._in_channels = 3
+#         Input: `x` with shape (1, 3, 64, 64)
+#         Output: [f0, f1, f2, f3, f4, f5] - features with corresponding shapes
+#                 [(1, 3, 64, 64), (1, 64, 32, 32), (1, 128, 16, 16), (1, 256, 8, 8),
+#                 (1, 512, 4, 4), (1, 1024, 2, 2)] (C - dim may differ)
 
-        # Define encoder modules below
-        self.encoder = backbone
+#         also should support number of features according to specified depth, e.g. if depth = 5,
+#         number of feature tensors = 6 (one with same resolution as input and 5 downsampled),
+#         depth = 3 -> number of feature tensors = 4 (one with same resolution as input and 3 downsampled).
+#         """
+#         feats = [self.concatenate(x1, x2)]
+#         for i, module in enumerate(self.encoder.children()):
+#             x1 = module(x1)
+#             x2 = module(x2)
+#             if i in self.feature_indices:
+#                 feats.append(self.concatenate(x1, x2))
+#             if i == self.feature_indices[-1]:
+#                 break
 
-        self.diff = diff
+#         return feats
 
-    def forward(self, x1, x2):
-        """Produce list of features of different spatial resolutions, each feature is a 4D torch.tensor of
-        shape NCHW (features should be sorted in descending order according to spatial resolution, starting
-        with resolution same as input `x` tensor).
-
-        Input: `x` with shape (1, 3, 64, 64)
-        Output: [f0, f1, f2, f3, f4, f5] - features with corresponding shapes
-                [(1, 3, 64, 64), (1, 64, 32, 32), (1, 128, 16, 16), (1, 256, 8, 8),
-                (1, 512, 4, 4), (1, 1024, 2, 2)] (C - dim may differ)
-
-        also should support number of features according to specified depth, e.g. if depth = 5,
-        number of feature tensors = 6 (one with same resolution as input and 5 downsampled),
-        depth = 3 -> number of feature tensors = 4 (one with same resolution as input and 3 downsampled).
-        """
-        feats = [self.concatenate(x1, x2)]
-        for i, module in enumerate(self.encoder.children()):
-            x1 = module(x1)
-            x2 = module(x2)
-            if i in self.feature_indices:
-                feats.append(self.concatenate(x1, x2))
-            if i == self.feature_indices[-1]:
-                break
-
-        return feats
-
-    def concatenate(self, x1, x2):
-        if self.diff:
-            return torch.abs(x1 - x2)
-        else:
-            torch.cat([x1, x2], 1)
+#     def concatenate(self, x1, x2):
+#         if self.diff:
+#             return torch.abs(x1 - x2)
+#         else:
+#             torch.cat([x1, x2], 1)
 
 
 class DoubleConv(nn.Module):
