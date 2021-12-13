13f211931de45753fdbc9d4d36bc333de0e22119
Toolkit User <pau.rodri1@gmail.com>
2021-12-13 20:53:44 +0000

    use train loss generator

--- toolbox/tests/test_toolbox.py
+++ toolbox/tests/test_toolbox.py
@@ -3,7 +3,7 @@ import torch
 import torchvision
 import torchvision.transforms as tt
 from toolbox.model import Model
-from toolbox import TaskSpecifications, head_generator
+from toolbox import TaskSpecifications, head_generator, train_loss_generator
 from toolbox.backbone import Conv4Example
 import pytorch_lightning as pl
 
@@ -11,10 +11,11 @@ import pytorch_lightning as pl
 def test_toolbox_mnist():
     hyperparameters = {'lr_milestones': [10, 20],
                        'lr_gamma': 0.1,
-                       'lr_backbone': 1e-4,
-                       'lr_head': 1e-4,
+                       'lr_backbone': 1e-3,
+                       'lr_head': 1e-3,
                        'head_type': 'linear',
                        'train_iters': 100,
+                       'loss_type': 'crossentropy'
                        }
     specs = TaskSpecifications((28, 28, 1, 1),
                                features_shape=(64,),
@@ -24,15 +25,16 @@ def test_toolbox_mnist():
 
     head = head_generator(specs, hyperparameters)
     backbone = Conv4Example('./', specs, hyperparameters)
+    criterion = train_loss_generator(specs, hyperparameters)
     model = Model(backbone,
                   head,
-                  torch.nn.CrossEntropyLoss(),
+                  criterion,
                   hyperparameters=hyperparameters)
 
     t = tt.ToTensor()
     train_dataset = torchvision.datasets.MNIST('/mnt/public/datasets/mnist',
                                                transform=t)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=1)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=1)
 
     trainer = pl.Trainer(gpus=0, max_epochs=1, max_steps=hyperparameters['train_iters'], logger=False)
     trainer.fit(model, train_dataloaders=train_loader)
