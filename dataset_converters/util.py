49ffbf5f4c342f95f21a30a7101ca309f111bb66
Toolkit User <gbxhuang@gmail.com>
2022-01-04 18:21:47 +0000

    read dataset paths from environment if possible

--- dataset_converters/util.py
+++ dataset_converters/util.py
@@ -1,5 +1,5 @@
 import os
 
 # TODO replace by environment variable CC_BENCHMARK_SOURCE_DATASETS
-src_datasets_dir = os.path.expanduser("~/dataset/")
-dst_datasets_dir = os.path.expanduser("~/converted_dataset/")
+src_datasets_dir = os.environ.get('CC_BENCHMARK_SOURCE_DATASETS', os.path.expanduser("~/dataset/"))
+dst_datasets_dir = os.environ.get('CC_BENCHMARK_CONVERTED_DATASETS', os.path.expanduser("~/converted_dataset/"))
