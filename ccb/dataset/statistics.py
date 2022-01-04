fc85fefe71acbec952b85f3c7ac59c2579a90a0f
recursix <alex.lacoste.shmu@gmail.com>
2022-01-04 11:55:27 -0500

    Adapting brick kiln to the new format.

--- ccb/dataset/statistics.py
+++ ccb/dataset/statistics.py
@@ -1,11 +1,10 @@
-
 from ccb.dataset import io
 from collections import Counter, defaultdict
 import numpy as np
 from tqdm import tqdm
-import seaborn as sns
 from matplotlib import pyplot as plt
 from ipyleaflet import Map, Marker, projections, Rectangle
+import math
 
 
 def compare(a, b, name, src_a, src_b):
@@ -13,41 +12,22 @@ def compare(a, b, name, src_a, src_b):
         print(f"Consistancy error with {name} between:\n    {src_a}\n  & {src_b}.\n    {str(a)}\n != {str(b)}")
 
 
-def dataset_statistics(dataset_iterator, n_value_per_images=100, iterator_len=None):
+def dataset_statistics(dataset_iterator, n_value_per_images=1000, iterator_len=None):
 
     label_counter = Counter()
 
     band_stats = defaultdict(list)
 
-    for i, sample in enumerate(dataset_iterator):
+    for i, sample in enumerate(tqdm(dataset_iterator)):
 
-        band_count = sample.band_count()
-        lenghts = np.array((len(sample.band_names), len(sample.band_wavelength), sample.image.shape[2]))
-        if np.any(band_count != lenghts):
-            raise ValueError(
-                f"Inconsistant number of bands in {sample.name}. Note, the image shape should be (height, width, bands).")
+        for band in sample.bands:
+            band_name = band.band_info.name
+            band_stats[band_name].append(np.random.choice(band.data.flat, size=n_value_per_images, replace=False))
 
-        if i == 0:
-
-            spatial_resolution = sample.spatial_resolution
-            band_names = sample.band_names
-            crs = sample.crs
-            first_name = sample.name
-
-            # TODO:
-            #  * band_wavelength
-            #  * meta_info
-
+        if isinstance(sample.label, io.Band):
+            band_stats["label"].append(np.random.choice(sample.label.data.flat, size=n_value_per_images, replace=False))
         else:
-            compare(spatial_resolution, sample.spatial_resolution, "spatial_resolution", first_name, sample.name)
-            compare(tuple(band_names), tuple(sample.band_names), "band_names", first_name, sample.name)
-            compare(crs, sample.crs, "crs", first_name, sample.name)
-
-        for idx, band_name in enumerate(band_names):
-            band_stats[band_name].append(np.random.choice(sample.get_band(idx).flat,
-                                                          size=n_value_per_images, replace=False))
-
-        label_counter[sample.label] += 1
+            label_counter[sample.label] += 1
 
     band_values = {}
     for band_name, values in band_stats.items():
@@ -63,7 +43,9 @@ def print_stats(label_counter):
 
 
 def plot_band_stats(band_values):
-    keys, values = zip(*band_values.items())
+    items = list(band_values.items())
+    items.sort(key=lambda item: item[0])
+    keys, values = zip(*items)
     fig1, ax = plt.subplots()
     ax.set_title("Band Statistics")
     ax.violinplot(dataset=values, vert=False)
@@ -72,56 +54,99 @@ def plot_band_stats(band_values):
     ax.set_yticklabels(labels=keys)
 
 
-def extract_images(samples, bands=(3, 2, 1), percentile_max=99.9):
-    images = []
-    labels = []
-    for sample in samples:
-        img_data = sample.image[:, :, bands].astype(np.float)
-        images.append(img_data)
-        labels.append(sample.label)
+def plot_band_stats2(band_values, n_cols=4):
+    items = list(band_values.items())
+    items.sort(key=lambda item: item[0])
+    n_rows = int(math.ceil(len(items) / n_cols))
+    fig1, ax_matrix = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(20, 10))
+    for i, (key, value) in enumerate(tqdm(items)):
+        ax = ax_matrix.flat[i]
+        ax.set_title(key)
+        ax.hist(value, bins="auto")
+    plt.tight_layout()
+
+
+def float_image_to_uint8(images, percentile_max=99.9):
+    """Convert a batch of images to uint8 such that 99.9% of values fit in the range (0,255)."""
+    images = np.asarray(images)
+    if images.dtype == np.uint8:
+        return images
+
+    if np.any(images < 0):
+        raise ValueError("Images contain negative values. Can't conver to uint8")
+
+    images = images.astype(np.float64)
 
     mx = np.percentile(images, q=percentile_max)
     new_images = []
     for image in images:
-        image = np.clip(image * 255 / mx, 0, 255)
+        image = np.clip(image * (255 / mx), 0, 255)
         new_images.append(image.astype(np.uint8))
-    return new_images, labels
+    return new_images
+
+
+def extract_images(samples, band_names=("red", "green", "blue"), percentile_max=99.9, resample=False, fill_value=None):
+    images = []
+    labels = []
+    for sample in samples:
+        img_data, _, _ = sample.pack_to_4d(sample.dates[:1], band_names, resample=resample, fill_value=fill_value)
+        img_data = img_data[0].astype(np.float)
+        images.append(img_data)
+        labels.append(sample.label)
+
+    images = float_image_to_uint8(images, percentile_max)
+    return images, labels
+
+
+def extract_label_as_image(samples, percentile_max=99.9):
+    images = []
+    for sample in samples:
+        if not isinstance(sample.label, io.Band):
+            raise ValueError("sample.label must be of type Band")
+
+        images.append(sample.label.data)
+    return float_image_to_uint8(images, percentile_max)
 
 
 def extract_bands(samples):
     band_names = samples[0].band_names
-    # band_names = [f"{i:02d} {band_name}" for i, band_name in enumerate(band_names)]
     all_images = []
     labels = []
     for i, band_name in enumerate(band_names):
-        images, _ = extract_images(samples, i)
+        images, _ = extract_images(samples, band_names=(band_name,))
+        images = [image[:, :, 0] for image in images]
         all_images.extend(images)
-        labels.extend((band_name, ) * len(images))
+        labels.extend((band_name,) * len(images))
+
+    if isinstance(samples[0].label, io.Band):
+        label_images = extract_label_as_image(samples)
+        all_images.extend(label_images)
+        labels.extend(("label",) * len(label_images))
 
     return all_images, labels
 
 
-def center_coord(sample):
+def center_coord(band):
     # TODO why do I have to reverse lon,lat ?
-    center = np.array(sample.image.shape[:2]) / 2.
-    return tuple((sample.transform * center)[::-1])
+    center = np.array(band.data.shape[:2]) / 2.0
+    return tuple((band.transform * center)[::-1])
 
 
-def get_rect(sample):
-    sw = sample.transform * (0, 0)
-    ne = sample.transform * sample.image.shape[:2]
+def get_rect(band):
+    sw = band.transform * (0, 0)
+    ne = band.transform * band.data.shape[:2]
     return Rectangle(bounds=(sw[::-1], ne[::-1]))
 
 
 def leaflet_map(samples):
 
-    map = Map(center=center_coord(samples[0]), zoom=7)
-    map.layout.height = '800px'
+    map = Map(center=center_coord(samples[0].bands[0]), zoom=7)
+    map.layout.height = "800px"
 
     for sample in tqdm(samples):
-
-        map.add_layer(Marker(location=center_coord(sample), draggable=False,
-                             opacity=0.5, title=sample.name, alt=sample.name))
-        map.add_layer(get_rect(sample))
+        band = sample.bands[0]
+        name = ""
+        map.add_layer(Marker(location=center_coord(band), draggable=False, opacity=0.5, title=name, alt=name))
+        map.add_layer(get_rect(band))
 
     return map
