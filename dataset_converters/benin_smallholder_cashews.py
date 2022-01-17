61b5f46939f2ee086aaa4b1f77727aae66c53c39
Toolkit User <gbxhuang@gmail.com>
2022-01-17 19:22:47 +0000

    finish benin

--- dataset_converters/benin_smallholder_cashews.py
+++ dataset_converters/benin_smallholder_cashews.py
@@ -10,8 +10,9 @@ from pathlib import Path
 from tqdm import tqdm
 from toolbox.core.task_specs import TaskSpecifications
 from toolbox.core.loss import SegmentationAccuracy
+import torchgeo
 from torchgeo.datasets import BeninSmallHolderCashews
-
+import datetime
 
 # Classification labels
 LABELS = (
@@ -23,6 +24,81 @@ LABELS = (
     'background',
     'uncertain'
 )
+DATES = (
+        "2019-11-05",
+        "2019-11-10",
+        "2019-11-15",
+        "2019-11-20",
+        "2019-11-30",
+        "2019-12-05",
+        "2019-12-10",
+        "2019-12-15",
+        "2019-12-20",
+        "2019-12-25",
+        "2019-12-30",
+        "2020-01-04",
+        "2020-01-09",
+        "2020-01-14",
+        "2020-01-19",
+        "2020-01-24",
+        "2020-01-29",
+        "2020-02-08",
+        "2020-02-13",
+        "2020-02-18",
+        "2020-02-23",
+        "2020-02-28",
+        "2020-03-04",
+        "2020-03-09",
+        "2020-03-14",
+        "2020-03-19",
+        "2020-03-24",
+        "2020-03-29",
+        "2020-04-03",
+        "2020-04-08",
+        "2020-04-13",
+        "2020-04-18",
+        "2020-04-23",
+        "2020-04-28",
+        "2020-05-03",
+        "2020-05-08",
+        "2020-05-13",
+        "2020-05-18",
+        "2020-05-23",
+        "2020-05-28",
+        "2020-06-02",
+        "2020-06-07",
+        "2020-06-12",
+        "2020-06-17",
+        "2020-06-22",
+        "2020-06-27",
+        "2020-07-02",
+        "2020-07-07",
+        "2020-07-12",
+        "2020-07-17",
+        "2020-07-22",
+        "2020-07-27",
+        "2020-08-01",
+        "2020-08-06",
+        "2020-08-11",
+        "2020-08-16",
+        "2020-08-21",
+        "2020-08-26",
+        "2020-08-31",
+        "2020-09-05",
+        "2020-09-10",
+        "2020-09-15",
+        "2020-09-20",
+        "2020-09-25",
+        "2020-09-30",
+        "2020-10-10",
+        "2020-10-15",
+        "2020-10-20",
+        "2020-10-25",
+        "2020-10-30",
+    )
+DATES = [datetime.datetime.strptime(date, "%Y-%m-%d").date() for date in DATES]
+
+
 SPATIAL_RESOLUTION = 0.5  # meters, to be confirmed
 N_TIMESTEPS = 70
 LABEL_BAND = io.SegmentationClasses("label", spatial_resolution=SPATIAL_RESOLUTION, n_classes=len(LABELS))
@@ -49,6 +125,7 @@ def make_sample(images, mask, sample_name):
             band = io.Band(
                 data=band_data,
                 band_info=band_info,
+                date=DATES[date_idx],
                 spatial_resolution=SPATIAL_RESOLUTION,
                 transform=transform,
                 crs=crs,
@@ -82,8 +159,8 @@ if __name__ == "__main__":
     for i, tg_sample in enumerate(tqdm(cashew)):
         sample_name = f"sample={i:04d}"
 
-        images = np.array(tg_sample["image"])
-        mask = tg_sample["mask"]
+        images = tg_sample["image"].numpy()
+        mask = tg_sample["mask"].numpy()
 
         sample = make_sample(images, mask, sample_name)
         sample.write(dataset_dir)
