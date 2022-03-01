3bd61fb7edff336b031c2a815a3e400627dfd219
Gabriel Huang <gbxhuang@gmail.com>
2022-03-01 02:37:31 +0000

    auto-load all stats for Dataset() if exist

--- ccb/io/bandstats.py
+++ ccb/io/bandstats.py
@@ -24,7 +24,7 @@ def main(args):
             print(f'Computing statistics for {partition}:{split}')
             stats = dataset_statistics2(dataset, n_value_per_image=1000, n_samples=100)
             stats_fname = os.path.join(args.dataset, f'{partition}_{split}_bandstats.json')
-            with open(stats_fname, 'w') as fp:
+            with open(stats_fname, 'w', encoding='utf8') as fp:
                 json.dump(stats, fp)
             print('-> Dumped statistics to {}'.format(stats_fname))
 
