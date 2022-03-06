3e5efd9f404c2898c1a62b9b775bc31720b80e58
Alexandre Drouin <alexandre.drouin1@gmail.com>
2022-03-06 21:47:12 +0000

    More explicit experiment directory naming

--- ccb/experiment/experiment_generator.py
+++ ccb/experiment/experiment_generator.py
@@ -6,6 +6,7 @@ Usage: experiment_generator.py --model-generator path/to/my/model/generator.py  
 """
 import argparse
 
+from datetime import datetime
 from pathlib import Path
 from uuid import uuid4
 
@@ -19,7 +20,8 @@ def experiment_generator(
     experiment_dir: str,
     task_filter: callable = None,
     max_num_configs: int = 10,
-    benchmark_name="default",
+    benchmark_name: str = "default",
+    experiment_name: str = None,
 ):
     """
     Generates the directory structure for every tasks and every hyperparameter configuration.
@@ -33,10 +35,14 @@ def experiment_generator(
         The directory in which to create the experiment directories.
     task_filter: callable(TaskSpecification)
         A function that takes as input a task specification instance and returns False if it should be skipped.
+    benchmark_name: str
+        The name of the benchmark on which to conduct the experiment (default: "default").
+    experiment_name: str
+        The name of the current experiment. Will be used as a prefix to the results directory (default: None).
 
     """
     experiment_dir = Path(experiment_dir)
-    experiment_dir /= str(uuid4())
+    experiment_dir /= f"{experiment_name + '_' if experiment_name is not None else ''}{benchmark_name}_{datetime.now().strftime('%m-%d-%Y_%H:%M:%S')}"
 
     model_generator = get_model_generator(model_generator_module_name)
 
@@ -79,10 +85,19 @@ def start():
         default="default",
     )
 
+    parser.add_argument(
+        "--experiment-name",
+        help="An optional name to give to the experiment. Will be used as a prefix to the results directory.",
+        required=False,
+        default=None,
+    )
+
     args = parser.parse_args()
 
     # Generate experiments
-    experiment_generator(args.model_generator, args.experiment_dir, benchmark_name=args.benchmark)
+    experiment_generator(
+        args.model_generator, args.experiment_dir, benchmark_name=args.benchmark, experiment_name=args.experiment_name
+    )
 
 
 if __name__ == "__main__":
