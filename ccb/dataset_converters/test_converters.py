8364e6daf6181e6d401b1f0de5cc0761351ef2da
recursix <alex.lacoste.shmu@gmail.com>
2022-03-08 21:22:39 +0000

    skipif

--- ccb/dataset_converters/test_converters.py
+++ ccb/dataset_converters/test_converters.py
@@ -1,4 +1,4 @@
-from ccb.io.dataset import Dataset, check_dataset_integrity
+from ccb import io
 from pathlib import Path
 import tempfile
 import pytest
@@ -11,54 +11,57 @@ def converter_tester(converter):
     with tempfile.TemporaryDirectory() as datasets_dir:
         dataset_dir = Path(datasets_dir, converter.DATASET_NAME)
         converter.convert(max_count=5, dataset_dir=Path(dataset_dir))
-        dataset = Dataset(dataset_dir)
+        dataset = io.Dataset(dataset_dir)
         assert len(dataset) == 5
-        check_dataset_integrity(dataset)
+        io.check_dataset_integrity(dataset)
 
 
-@pytest.mark.skip(reason="Requires presence of the source datasets.")
+SRC_DIR_EXISTS = not Path(io.src_datasets_dir).exists()
+
+
+@pytest.mark.skipif(SRC_DIR_EXISTS, reason="Requires presence of the source datasets.")
 def test_brick_kiln():
     from ccb.dataset_converters import brick_kiln
 
     converter_tester(brick_kiln)
 
 
-@pytest.mark.skip(reason="Requires presence of the source datasets.")
+@pytest.mark.skipif(SRC_DIR_EXISTS, reason="Requires presence of the source datasets.")
 def test_cv4a_kenya_cropy_type():
     from ccb.dataset_converters import cv4a_kenya_crop_type
 
     converter_tester(cv4a_kenya_crop_type)
 
 
-@pytest.mark.skip(reason="Requires presence of the source datasets.")
+@pytest.mark.skipif(SRC_DIR_EXISTS, reason="Requires presence of the source datasets.")
 def test_eurosat():
     from ccb.dataset_converters import eurosat
 
     converter_tester(eurosat)
 
 
-@pytest.mark.skip(reason="Requires presence of the source datasets.")
+@pytest.mark.skipif(SRC_DIR_EXISTS, reason="Requires presence of the source datasets.")
 def test_neon_tree():
     from ccb.dataset_converters import neon_tree
 
     converter_tester(neon_tree)
 
 
-@pytest.mark.skip(reason="Requires presence of the source datasets.")
+@pytest.mark.skipif(SRC_DIR_EXISTS, reason="Requires presence of the source datasets.")
 def test_smallholder_cashews():
     from ccb.dataset_converters import benin_smallholder_cashews
 
     converter_tester(benin_smallholder_cashews)
 
 
-@pytest.mark.skip(reason="Requires presence of the source datasets.")
+@pytest.mark.skipif(SRC_DIR_EXISTS, reason="Requires presence of the source datasets.")
 def test_so2sat():
     from ccb.dataset_converters import so2sat
 
     converter_tester(so2sat)
 
 
-@pytest.mark.skip(reason="Requires presence of the source datasets.")
+@pytest.mark.skipif(SRC_DIR_EXISTS, reason="Requires presence of the source datasets.")
 def test_nz_cattle_detection():
     from ccb.dataset_converters import nz_cattle_detection
 
