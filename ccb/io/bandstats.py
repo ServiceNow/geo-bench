94675e6e26a474b7dd38d5797aad13e97acb9c30
Gabriel Huang <gbxhuang@gmail.com>
2022-03-10 22:51:51 +0000

    fix bandstats: use Stat class, display available stats in Dataset __repr__, pick split_wise or global stats

--- ccb/io/bandstats.py
+++ ccb/io/bandstats.py
@@ -7,26 +7,45 @@ import argparse
 import json
 import os
 
-from ccb.io.dataset import Dataset, dataset_statistics, dataset_statistics2
+from ccb.io.dataset import Dataset, dataset_statistics
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("dataset", help="path to CCB dataset folder")
+parser.add_argument("-s", "--splits", default=False, help="compute statistics separately for each split")
+parser.add_argument("-c", "--check", default=False, help="don't compute statistics, but load and check only")
 
 
 def main(args):
     print("Loading dataset", args.dataset)
     dataset = Dataset(args.dataset)
     print(dataset)
-    for partition in dataset.list_partitions():
-        dataset.set_active_partition(partition)
-        for split in dataset.list_splits():
-            print(f"Computing statistics for {partition}:{split}")
-            stats = dataset_statistics2(dataset, n_value_per_image=1000, n_samples=100)
-            stats_fname = os.path.join(args.dataset, f"{partition}_{split}_bandstats.json")
+    if not args.check:
+        if args.splits:
+            for partition in dataset.list_partitions():
+                dataset.set_active_partition(partition)
+                for split in dataset.list_splits():
+                    print(f"Computing statistics for {partition}:{split}")
+                    dataset.set_split(split)
+                    band_values, band_stats = dataset_statistics(dataset, n_value_per_image=1000, n_samples=100)
+                    stats_fname = os.path.join(args.dataset, f"{partition}_{split}_bandstats.json")
+                    with open(stats_fname, "w", encoding="utf8") as fp:
+                        json.dump({k:v.to_dict() for k,v in band_stats.items()}, fp)
+                    print("-> Dumped statistics to {}".format(stats_fname))
+        else:
+            dataset.set_active_partition('default')
+            dataset.set_split(None)
+            print(f"Computing single statistics for whole dataset")
+            band_values, band_stats = dataset_statistics(dataset, n_value_per_image=1000, n_samples=100)
+            stats_fname = os.path.join(args.dataset, f"all_bandstats.json")
             with open(stats_fname, "w", encoding="utf8") as fp:
-                json.dump(stats, fp)
+                json.dump({k:v.to_dict() for k,v in band_stats.items()}, fp)
             print("-> Dumped statistics to {}".format(stats_fname))
+        
+
+
+
+
 
 
 if __name__ == "__main__":
