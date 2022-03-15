793049bf2030acf4e36d12f11f442edf9dc6772d
recursix <alex.lacoste.shmu@gmail.com>
2022-03-15 00:22:00 +0000

    remove multiprocess

--- ccb/dataset_converters/benin_smallholder_cashews.py
+++ ccb/dataset_converters/benin_smallholder_cashews.py
@@ -143,8 +143,7 @@ def make_sample(images, mask, sample_name):
     return io.Sample(bands, label=label, sample_name=sample_name)
 
 
-def process_i(args):
-    i, tg_sample, dataset_dir = args
+def convert_sample(i, tg_sample, dataset_dir):
     sample_name = f"sample_{i:08d}"
 
     images = tg_sample["image"].numpy()
@@ -173,20 +172,25 @@ def convert(max_count=None, dataset_dir=DATASET_DIR):
         # either 50cm or 40cm, Airbus Pleiades 50cm, https://radiantearth.blob.core.windows.net/mlhub/technoserve-cashew-benin/Documentation.pdf
         spatial_resolution=SPATIAL_RESOLUTION,
     )
-    task_specs.save(dataset_dir)
+    task_specs.save(dataset_dir, overwrite=True)
 
-    multiprocess = True
-    if multiprocess:
-        with Pool(os.cpu_count()) as p:
-            print(f"Spinning up pool of {os.cpu_count()} workers")
-            iterator = p.imap(process_i, ((i, cashew_i, dataset_dir) for i, cashew_i in enumerate(cashew)))
-            for i in tqdm(iterator, total=len(cashew)):
-                pass
+    # multiprocess = False
+    # if multiprocess:
+    #     with Pool(os.cpu_count()) as p:
+    #         print(f"Spinning up pool of {os.cpu_count()} workers")
+    #         iterator = p.imap(convert_sample, ((i, cashew_i, dataset_dir) for i, cashew_i in enumerate(cashew)))
+    #         for i in tqdm(iterator, total=len(cashew)):
+    #             pass
 
-    else:
-        for i, cashew_i in enumerate(tqdm(cashew)):
-            process_i(i, cashew_i, dataset_dir)
+    # else:
+    # for i, data in enumerate(tqdm(cashew)):
+    i = 0
+    for data in cashew:
+        i += 1
+        if i + 1 >= max_count:
+            break
+        convert_sample(i, data, dataset_dir)
 
 
 if __name__ == "__main__":
-    convert()
+    convert(10)
