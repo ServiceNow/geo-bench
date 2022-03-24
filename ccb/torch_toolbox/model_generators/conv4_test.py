de4b53f18129da1db5cfd2029ec5b8dc3b17e386
recursix <alex.lacoste.shmu@gmail.com>
2022-03-24 14:25:53 +0000

    try to make test faster

--- ccb/torch_toolbox/model_generators/conv4_test.py
+++ ccb/torch_toolbox/model_generators/conv4_test.py
@@ -5,7 +5,8 @@ from ccb.torch_toolbox.model_generators import conv4
 class Conv4GeneratorTest(conv4.Conv4Generator):
     def hp_search(self, task_specs, max_num_configs=10):
         hparams1 = self.base_hparams.copy()
-        hparams1["train_iters"] = 50
+        hparams1["train_iters"] = 10
+        hparams1["num_workers"] = 0
         hparams1["n_gpus"] = 0
 
         hparams2 = hparams1.copy()
