bbc6a0eee999b9901eb05899bf0e760c0b91f95c
recursix <alex.lacoste.shmu@gmail.com>
2022-03-28 20:13:28 +0000

    solve push bug

--- toolkit/exp_launcher.py
+++ toolkit/exp_launcher.py
@@ -6,15 +6,17 @@
 from pathlib import Path
 from ccb.experiment.experiment_generator import experiment_generator
 from toolkit import dispatch_toolkit
+import time
 
 
 expirment_dir = experiment_generator(
     model_generator_module_name="ccb.torch_toolbox.model_generators.conv4",
     experiment_dir="/mnt/data/experiments/allac",  # make sure this datamodule is mounted like this: snow.rg_climate_benchmark.data:/mnt/data
     benchmark_name="classification",
-    experiment_name="test_conv4",
+    experiment_name="basic_conv4",
 )
 
-
 dispatch_toolkit.push_code(Path(__file__).parent.parent)
-dispatch_toolkit.toolkit_dispatcher(expirment_dir)
+
+# you may want to change to your WANDB_API_KEY."
+dispatch_toolkit.toolkit_dispatcher(expirment_dir, env_vars=("WANDB_API_KEY=af684d249ec704e48f0cd23c37d683bd388c0efd",))
