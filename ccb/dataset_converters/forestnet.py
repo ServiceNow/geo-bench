a5eb11051a4e628a430e6b7d1870b19dfec6dee3
recursix <alex.lacoste.shmu@gmail.com>
2022-04-01 20:16:02 +0000

    reviewing forestnet and modifying minor things and black

--- ccb/dataset_converters/forestnet.py
+++ ccb/dataset_converters/forestnet.py
@@ -3,8 +3,6 @@
 # Unzip the directory, then either place contents in dataset/forestnet_v1.0
 # or create a symlink.
 import sys
-import csv
-import h5py
 import pickle
 import rasterio
 import datetime
@@ -13,8 +11,6 @@ import pandas as pd
 from tqdm import tqdm
 from PIL import Image, ImageDraw
 from pathlib import Path
-
-sys.path.append(str(Path.cwd()))
 from ccb import io
 
 
@@ -23,7 +19,7 @@ SRC_DATASET_DIR = Path(io.src_datasets_dir) / DATASET_NAME
 DATASET_DIR = Path(io.datasets_dir) / DATASET_NAME
 SPATIAL_RESOLUTION = 15
 PATCH_SIZE = 332
-LABELS =  [
+LABELS = [
     "Ignore",
     "Oil palm plantation",
     "Timber plantation",
@@ -36,67 +32,45 @@ LABELS =  [
     "Fish pond",
     "Logging",
     "Secondary forest",
-    "Other"
+    "Other",
 ]
 LABEL_BAND = io.SegmentationClasses(
-    "label",
-    spatial_resolution=SPATIAL_RESOLUTION,
-    n_classes=len(LABELS),
-    class_names=LABELS
+    "label", spatial_resolution=SPATIAL_RESOLUTION, n_classes=len(LABELS), class_names=LABELS
 )
 LABEL_IGNORE_VALUE = 0
 
 
 def draw_img_roi(draw, shape, label: int):
     shape_type = shape.geom_type
-    if shape_type == 'Polygon':
+    if shape_type == "Polygon":
         coords = np.array(shape.exterior.coords)
-        draw.polygon([tuple(coord) for coord in coords],
-                     outline=label, fill=label)
+        draw.polygon([tuple(coord) for coord in coords], outline=label, fill=label)
     else:
         for poly in shape.geoms:
             coords = np.array(poly.exterior.coords)
-            draw.polygon([tuple(coord) for coord in coords],
-                         outline=label, fill=label)
+            draw.polygon([tuple(coord) for coord in coords], outline=label, fill=label)
 
 
 def load_sample(example_dir: Path, label: str, year: int):
     # Get lat center and lon center from img path
     lat_center, lon_center = map(float, example_dir.name.split("_"))
 
-    transform_center = rasterio.transform.from_origin(
-        lon_center,
-        lat_center,
-        SPATIAL_RESOLUTION,
-        SPATIAL_RESOLUTION
-    )
-    lon_corner, lat_corner = (
-        transform_center * [-PATCH_SIZE//2, -PATCH_SIZE//2]
-    )
-    transform = rasterio.transform.from_origin(
-        lon_corner,
-        lat_corner,
-        SPATIAL_RESOLUTION,
-        SPATIAL_RESOLUTION
-    )
+    transform_center = rasterio.transform.from_origin(lon_center, lat_center, SPATIAL_RESOLUTION, SPATIAL_RESOLUTION)
+    lon_corner, lat_corner = transform_center * [-PATCH_SIZE // 2, -PATCH_SIZE // 2]
+    transform = rasterio.transform.from_origin(lon_corner, lat_corner, SPATIAL_RESOLUTION, SPATIAL_RESOLUTION)
 
     # Load the visible + infrared images and add them as bands
     images_dir = example_dir / "images"
     visible_dir = images_dir / "visible"
-    infrared_dir = images_dir / "infrared"
     crs = "EPSG:4326"
     bands = []
     seen_dates = set()
     for visible_image_path in visible_dir.iterdir():
-        infrared_path = Path(
-            str(visible_image_path)
-            .replace("visible", "infrared")
-            .replace("png", "npy")
-        )
+        infrared_path = Path(str(visible_image_path).replace("visible", "infrared").replace("png", "npy"))
         is_composite = visible_image_path.stem == "composite"
         if is_composite:
             # Assign last possible date to composite image
-            date = f'{max(year, 2012) + 5}_01_01'
+            date = f"{max(year, 2012) + 5}_01_01"
             clouds = None
         else:
             date, clouds = visible_image_path.stem.split("_cloud_")
@@ -110,10 +84,7 @@ def load_sample(example_dir: Path, label: str, year: int):
         seen_dates.add(date)
         visible_img = np.array(Image.open(visible_image_path).convert("RGB"))
         infrared_img = np.load(infrared_path)
-        meta_info = {
-            "n_cloud_pixels": clouds,
-            "is_composite": is_composite
-        }
+        meta_info = {"n_cloud_pixels": clouds, "is_composite": is_composite}
         # Visible
         for i, band_idx in enumerate([3, 2, 1]):
             band_data = io.Band(
@@ -123,7 +94,7 @@ def load_sample(example_dir: Path, label: str, year: int):
                 spatial_resolution=SPATIAL_RESOLUTION,
                 transform=transform,
                 crs=crs,
-                meta_info=meta_info
+                meta_info=meta_info,
             )
             bands.append(band_data)
 
@@ -136,7 +107,7 @@ def load_sample(example_dir: Path, label: str, year: int):
                 spatial_resolution=SPATIAL_RESOLUTION,  # Infrared bands have been upsampled to 15m
                 transform=transform,
                 crs=crs,
-                meta_info=meta_info
+                meta_info=meta_info,
             )
             bands.append(band_data)
 
@@ -144,16 +115,12 @@ def load_sample(example_dir: Path, label: str, year: int):
     forest_loss_region = example_dir / "forest_loss_region.pkl"
     with forest_loss_region.open("rb") as f:
         forest_loss_polygon = pickle.load(f)
-    mask = Image.new('L', (PATCH_SIZE, PATCH_SIZE), LABEL_IGNORE_VALUE)
+    mask = Image.new("L", (PATCH_SIZE, PATCH_SIZE), LABEL_IGNORE_VALUE)
     label_int = LABELS.index(label)
     draw_img_roi(ImageDraw.Draw(mask), forest_loss_polygon, label_int)
 
     label = io.Band(
-        data=np.array(mask),
-        band_info=LABEL_BAND,
-        spatial_resolution=SPATIAL_RESOLUTION,
-        transform=transform,
-        crs=crs
+        data=np.array(mask), band_info=LABEL_BAND, spatial_resolution=SPATIAL_RESOLUTION, transform=transform, crs=crs
     )
 
     # How to add per-example metadata?
@@ -161,7 +128,7 @@ def load_sample(example_dir: Path, label: str, year: int):
     # TODO: Load the per pixel auxiliary files and add them as bands (*.npy)
     # TODO: Load the per image auxiliary files and add them as metadata? (*.json)
     # TODO: Load all files in NCEP and add them as metadata (ncep/*)
-    aux_dir = example_dir / "auxiliary"
+    # aux_dir = example_dir / "auxiliary"
 
     return io.Sample(bands, label=label, sample_name=example_dir.name)
 
@@ -174,12 +141,12 @@ def convert(max_count=None, dataset_dir=DATASET_DIR):
     task_specs = io.TaskSpecifications(
         dataset_name=DATASET_NAME,
         patch_size=(PATCH_SIZE, PATCH_SIZE),
-        n_time_steps=23, # Variable number of time steps, max 23 across the dataset
+        n_time_steps=23,  # Variable number of time steps, max 23 across the dataset
         bands_info=bands_info,
         bands_stats=None,  # Will be automatically written with inspect script
         label_type=LABEL_BAND,
-        eval_loss=io.SegmentationAccuracy,  # TODO probably not the final 
-                                            # loss eval loss. To be discussed.
+        eval_loss=io.SegmentationAccuracy,  # TODO probably not the final
+        # loss eval loss. To be discussed.
         spatial_resolution=SPATIAL_RESOLUTION,
     )
     task_specs.save(dataset_dir, overwrite=True)
@@ -192,22 +159,19 @@ def convert(max_count=None, dataset_dir=DATASET_DIR):
             split = "valid"
         for _, row in tqdm(df.iterrows(), total=df.shape[0]):
             example_dir = SRC_DATASET_DIR / row["example_path"]
-            sample = load_sample(
-                example_dir,
-                row['label'],
-                row['year']
-            )
+            sample = load_sample(example_dir, row["label"], row["year"])
             sample_name = example_dir.name
             partition.add(split, sample_name)
             sample.write(dataset_dir)
             sample_count += 1
 
-            # temporary for creating small datasets for development purpose
             if max_count is not None and sample_count >= max_count:
                 break
 
-    partition.save(dataset_dir, "original")
+        if max_count is not None and sample_count >= max_count:
+            break
+    partition.save(dataset_dir, "original", as_default=True)
 
 
 if __name__ == "__main__":
-    convert(max_count=10)
+    convert()
