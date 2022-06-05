64e1cef80cac9765472ba8a6a21c4487f27a1f91
Toolkit User <nils.lehmann24@gmail.com>
2022-06-05 11:50:59 +0000

    add train segmentation metric

--- ccb/torch_toolbox/model_generators/py_segmentation_generator.py
+++ ccb/torch_toolbox/model_generators/py_segmentation_generator.py
@@ -33,7 +33,7 @@ class SegmentationGenerator(ModelGenerator):
         super().__init__()
         # These params are for unit tests, please set proper ones for real optimization
         self.base_hparams = {
-            "input_size": (3, 64, 64),  # FIXME
+            "input_size": (3, 256, 256),  # FIXME
             "pretrained": True,
             "lr_backbone": 1e-5,
             "lr_head": 1e-4,
@@ -42,16 +42,21 @@ class SegmentationGenerator(ModelGenerator):
             "loss_type": "crossentropy",
             "batch_size": 4,
             "num_workers": 1,
-            "max_epochs": 10,
+            "max_epochs": 500,
             "n_gpus": 1,
-            "logger": "csv",  # Set to wandb for wandb tracking
-            "encoder_type": "resnet18",
-            "decoder_type": "Unet",
+            "logger": "wandb",  # Set to wandb for wandb tracking
+            "encoder_type": "resnet50",
+            "decoder_type": "DeepLabV3",
             "decoder_weights": "imagenet",
             "enable_progress_bar": False,
             "log_segmentation_masks": False,  # Set to true for visualizing seg masks in wandb
-            "fast_dev_run": True,  # runs 1 train, 1 validation, and 1 test batch.
-            "format": "tif",
+            "fast_dev_run": False,  # runs 1 train, 1 validation, and 1 test batch.
+            "sweep_config_yaml_path": "/mnt/home/climate-change-benchmark/ccb/torch_toolbox/wandb/hparams_segmentation_resnet50_deeplabv3.yaml",
+            "num_agents": 4,
+            "num_trials_per_agent": 5,
+            "band_names": ["red", "green", "blue"],  # , "01", "05", "06", "07", "08", "08A", "09", "10", "11", "12"],
+            "image_size": 224,
+            "format": "hdf5",
         }
         if hparams is not None:
             self.base_hparams.update(hparams)
