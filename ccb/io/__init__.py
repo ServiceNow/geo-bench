931782cd280f720abc2bd8094d78b593c622763f
Toolkit User <nils.lehmann24@gmail.com>
2022-06-09 21:11:35 +0000

    passing tests locally

--- ccb/io/__init__.py
+++ ccb/io/__init__.py
@@ -1,3 +1,10 @@
+"""CCB io."""
+
 from ccb.io.dataset import *
 from ccb.io.task import *
 from ccb.io.label import *
+
+
+# # https://stackoverflow.com/questions/40018681
+# for module in __all__:
+#     globals()[module].__module__ = "ccb.io"
