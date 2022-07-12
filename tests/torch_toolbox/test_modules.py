acc5abdb90146de3b37ef24258e6cf25e29647a6
nilsleh <nils.lehmann24@gmail.com>
2022-07-12 15:27:31 +0200

    fix black error

--- tests/torch_toolbox/test_modules.py
+++ tests/torch_toolbox/test_modules.py
@@ -1,13 +1,21 @@
-import torch
 import pytest
-from ccb.torch_toolbox.modules import ClassificationHead
+import torch
 
+from ccb.torch_toolbox.modules import ClassificationHead
 
 BATCH_SIZE = 2
 NUM_CLASSES = 4
 IN_CHANNEL = 5
 
-@pytest.mark.parametrize("x", [torch.rand(size=(BATCH_SIZE, IN_CHANNEL)), [torch.rand(size=(BATCH_SIZE, IN_CHANNEL))], torch.rand(size=(BATCH_SIZE, IN_CHANNEL, IN_CHANNEL, IN_CHANNEL))])
+
+@pytest.mark.parametrize(
+    "x",
+    [
+        torch.rand(size=(BATCH_SIZE, IN_CHANNEL)),
+        [torch.rand(size=(BATCH_SIZE, IN_CHANNEL))],
+        torch.rand(size=(BATCH_SIZE, IN_CHANNEL, IN_CHANNEL, IN_CHANNEL)),
+    ],
+)
 def test_new_channel_init(x):
 
     cls_head = ClassificationHead(in_ch=IN_CHANNEL, num_classes=NUM_CLASSES, hidden_size=10)
