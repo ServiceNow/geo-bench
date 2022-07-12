3e45fe2eabc6627789bf9d9b71c6cbb72c2171d5
Toolkit User <nils.lehmann24@gmail.com>
2022-07-12 18:38:50 +0000

    remove seed test file

--- ccb/experiment/experiment_generator.py
+++ ccb/experiment/experiment_generator.py
@@ -88,7 +88,7 @@ def experiment_generator(
         elif experiment_type == "seeded_runs":
             NUM_SEEDS = 3
 
-            with open("/mnt/data/experiments/nils/classification_results/test_seeded_runs.json", "r") as f:
+            with open("/mnt/data/experiments/nils/classification_results/seeded_runs.json", "r") as f:
                 seed_run_dict = json.load(f)
 
             back_name = config["model"]["backbone"]
