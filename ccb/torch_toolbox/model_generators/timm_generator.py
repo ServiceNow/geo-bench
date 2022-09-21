fix rotation

ccb/torch_toolbox/model_generators/timm_generator.py
@@ -252,7 +252,7 @@ class TIMMGenerator(ModelGenerator):
         t.append(tt.ToTensor())
         t.append(tt.Normalize(mean=mean, std=std))
         if train:
-            t.append(tt.RandomApply(tt.RandomRotation(degrees=(90, 90)), p=0.5))
+            t.append(tt.RandomApply(torch.nn.ModuleList([tt.RandomRotation((90, 90))]), p=0.5))
             t.append(tt.RandomHorizontalFlip())
             t.append(tt.RandomVerticalFlip())
             t.append(tt.ColorJitter(0.1))