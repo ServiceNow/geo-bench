f5bc1ea8fe477c72ba19bac63089811277bc273b
Nils Lehmann <nils.lehmann24@gmail.com>
2023-02-11 20:48:17 +0100

    extract geo information

--- ccb/benchmark/dataset_converters/bigearthnet.py
+++ ccb/benchmark/dataset_converters/bigearthnet.py
@@ -1,7 +1,13 @@
 """Big Earth Net dataset."""
 from pathlib import Path
+from typing import Optional, Callable, Dict
+from torch import Tensor
+import torch
 
 import numpy as np
+import rasterio
+from torchgeo.datasets import BigEarthNet
+from rasterio.enums import Resampling
 from torchgeo.datasets import BigEarthNet
 from tqdm import tqdm
 
@@ -11,6 +17,71 @@ DATASET_NAME = "bigearthnet"
 SRC_DATASET_DIR = Path(io.src_datasets_dir, "bigearthnet")  # type: ignore
 DATASET_DIR = Path(io.datasets_dir, DATASET_NAME)  # type: ignore
 
+
+class GeoBigEarthNet(BigEarthNet):
+    """Overwrite BigEarthNet to get geo information."""
+    def __init__(
+        self,
+        root: str = "data",
+        split: str = "train",
+        bands: str = "all",
+        num_classes: int = 19,
+        transforms: Optional[Callable[[Dict[str, Tensor]], Dict[str, Tensor]]] = None,
+        download: bool = False,
+        checksum: bool = False,
+    ) -> None:
+        """Initialize a new BigEarthNet dataset instance.
+        Args:
+            root: root directory where dataset can be found
+            split: train/val/test split to load
+            bands: load Sentinel-1 bands, Sentinel-2, or both. one of {s1, s2, all}
+            num_classes: number of classes to load in target. one of {19, 43}
+            transforms: a function/transform that takes input sample and its target as
+                entry and returns a transformed version
+            download: if True, download dataset and store it in the root directory
+            checksum: if True, check the MD5 of the downloaded files (may be slow)
+        """
+        super().__init__(root, split, bands, num_classes, transforms, download, checksum)
+
+    def __getitem__(self, index: int) -> Dict[str, Tensor]:
+        """Return an index within the dataset.
+        Args:
+            index: index to return
+        Returns:
+            data and label at that index
+        """
+        image, crs, bounds = self._load_image(index)
+        label = self._load_target(index)
+        sample: Dict[str, Tensor] = {"image": image, "label": label, "crs": crs, "bounds": bounds}
+
+        if self.transforms is not None:
+            sample = self.transforms(sample)
+
+        return sample
+
+    def _load_image(self, index: int) -> Tensor:
+        """Load a single image.
+        Args:
+            index: index to return
+        Returns:
+            the raster image or target
+        """
+        paths = self._load_paths(index)
+        images = []
+        for path in paths:
+            # Bands are of different spatial resolutions
+            # Resample to (120, 120)
+            with rasterio.open(path) as dataset:
+                array = dataset.read(
+                    indexes=1,
+                    out_shape=self.image_size,
+                    out_dtype="int32",
+                    resampling=Resampling.bilinear,
+                )
+                images.append(array)
+        arrays: "np.typing.NDArray[np.int_]" = np.stack(images, axis=0)
+        tensor = torch.from_numpy(arrays).float()
+        return tensor, dataset.crs, dataset.bounds
 
 def make_sample(
     images: "np.typing.NDArray[np.int_]", label, sample_name: str, task_specs: io.TaskSpecifications
@@ -75,7 +146,7 @@ def convert(max_count=None, dataset_dir=DATASET_DIR) -> None:
     task_specs.save(dataset_dir, overwrite=True)
     n_samples = 0
     for split_name in ["train", "val", "test"]:
-        bigearthnet_dataset = BigEarthNet(
+        bigearthnet_dataset = GeoBigEarthNet(
             root=SRC_DATASET_DIR,
             split=split_name,
             bands="s2",
--- ccb/benchmark/dataset_converters/eurosat.py
+++ ccb/benchmark/dataset_converters/eurosat.py
@@ -5,6 +5,11 @@ import os
 from pathlib import Path
 
 import numpy as np
+from typing import Dict, Optional, Callable, Sequence, Tuple
+from torch import Tensor
+import torch
+import rasterio
+from torchvision.datasets import ImageFolder
 from torchgeo.datasets import EuroSAT
 from tqdm import tqdm
 
@@ -13,6 +18,88 @@ from ccb import io
 DATASET_NAME = "eurosat"
 SRC_DATASET_DIR = Path(io.src_datasets_dir, DATASET_NAME)  # type: ignore
 DATASET_DIR = Path(io.datasets_dir, DATASET_NAME)  # type: ignore
+
+
+class GeoEuroSAT(EuroSAT):
+
+    all_band_names = (
+        "B01",
+        "B02",
+        "B03",
+        "B04",
+        "B05",
+        "B06",
+        "B07",
+        "B08",
+        "B08A",
+        "B09",
+        "B10",
+        "B11",
+        "B12",
+    )
+
+    rgb_bands = ("B04", "B03", "B02")
+
+    BAND_SETS = {"all": all_band_names, "rgb": rgb_bands}
+
+    """Wrapper to extract geo information."""
+    def __init__(
+        self,
+        root: str = "data",
+        split: str = "train",
+        bands: Sequence[str] = BAND_SETS["all"],
+        transforms: Optional[Callable[[Dict[str, Tensor]], Dict[str, Tensor]]] = None,
+        download: bool = False,
+        checksum: bool = False,
+    ) -> None:
+        """Initialize a new EuroSAT dataset instance.
+        Args:
+            root: root directory where dataset can be found
+            split: one of "train", "val", or "test"
+            bands: a sequence of band names to load
+            transforms: a function/transform that takes input sample and its target as
+                entry and returns a transformed version
+            download: if True, download dataset and store it in the root directory
+            checksum: if True, check the MD5 of the downloaded files (may be slow)
+        """
+        super().__init__(root, split, bands, transforms, download, checksum)
+
+    def __getitem__(self, index: int) -> Dict[str, Tensor]:
+        """Return an index within the dataset.
+        Args:
+            index: index to return
+        Returns:
+            data and label at that index
+        """
+        image, label, crs, bounds = self._load_image(index)
+
+        image = torch.index_select(image, dim=0, index=self.band_indices).float()
+        sample = {"image": image, "label": label, "crs": crs, "bound": bounds}
+
+        if self.transforms is not None:
+            sample = self.transforms(sample)
+
+        return sample
+
+    def _load_image(self, index: int) -> Tuple[Tensor, Tensor]:
+        """Load a single image and it's class label.
+        Args:
+            index: index to return
+        Returns:
+            the image
+            the image class label
+        """
+        path, _ = self.imgs[0]
+        img, label = ImageFolder.__getitem__(self, index)
+        with rasterio.open(path, "r") as src:
+            crs = src.crs
+            bounds = src.bounds
+        array: "np.typing.NDArray[np.int_]" = np.array(img)
+        tensor = torch.from_numpy(array).float()
+        # Convert from HxWxC to CxHxW
+        tensor = tensor.permute((2, 0, 1))
+        label = torch.tensor(label)
+        return tensor, label, crs, bounds
 
 
 def make_sample(images: "np.typing.NDArray[np.float_]", label, sample_name: str) -> io.Sample:
@@ -74,7 +161,7 @@ def convert(max_count=None, dataset_dir=DATASET_DIR) -> None:
 
     sample_id = 0
     for split_name in ["train", "val", "test"]:
-        eurosat_dataset = EuroSAT(root=SRC_DATASET_DIR, split=split_name, transforms=None, download=True, checksum=True)
+        eurosat_dataset = GeoEuroSAT(root=SRC_DATASET_DIR, split=split_name, transforms=None, download=True, checksum=True)
         for tg_sample in tqdm(eurosat_dataset):
             sample_name = f"id_{sample_id:04d}"
 
