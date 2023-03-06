1f51eb8a751b3574181f24eef8de1e8ba16f6daa
recursix <alex.lacoste.shmu@gmail.com>
2023-03-06 16:16:16 +0000

    temporary for backward compatibility of task_specs pkl

--- geobench/io/__init__.py
+++ geobench/io/__init__.py
@@ -3,3 +3,12 @@
 from geobench.io.dataset import *
 from geobench.io.label import *
 from geobench.io.task import *
+
+import geobench.io.task as task
+import geobench.io.dataset as dataset
+import geobench.io.label as label
+
+import sys
+sys.modules["ccb.io.task"] = task
+sys.modules["ccb.io.dataset"] = dataset
+sys.modules["ccb.io.label"] = label
