fea83d40c2dc5a7d3425f20486a6718cd700793e
recursix <alex.lacoste.shmu@gmail.com>
2023-03-21 19:12:26 +0000

    eval_loss deprecated

--- geobench/benchmark/dataset_converters/bigearthnet.py
+++ geobench/benchmark/dataset_converters/bigearthnet.py
@@ -145,7 +145,7 @@ def convert(max_count=None, dataset_dir=DATASET_DIR) -> None:
         bands_info=io.sentinel2_13_bands[0:10] + io.sentinel2_13_bands[-2:],
         bands_stats=None,  # Will be automatically written with the inspect script
         label_type=io.MultiLabelClassification(43, class_names=BigEarthNet.class_sets[43]),
-        eval_loss=io.MultilabelAccuracy,
+        # eval_loss=io.MultilabelAccuracy,
         spatial_resolution=10,
     )
 
--- geobench/benchmark/dataset_converters/nz_cattle_detection.py
+++ geobench/benchmark/dataset_converters/nz_cattle_detection.py
@@ -128,7 +128,7 @@ def convert(max_count=None, dataset_dir=DATASET_DIR) -> None:
         bands_info=BAND_INFO_LIST,
         bands_stats=None,  # Will be automatically written with the inspect script
         label_type=label_type,
-        eval_loss=io.SegmentationAccuracy(),  # TODO decide on the loss
+        # eval_loss=io.SegmentationAccuracy(),  # TODO decide on the loss
         spatial_resolution=0.1,
     )
     task_specs.save(dataset_dir, overwrite=True)
