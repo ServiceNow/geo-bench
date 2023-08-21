d37dbcd43fd3cb417daaf28e7ec1b4409b4016e8
recursix <alex.lacoste.shmu@gmail.com>
2023-08-21 19:31:27 +0000

    better downloader

--- geobench/download_geobench.py
+++ geobench/download_geobench.py
@@ -1,13 +1,13 @@
+#!/usr/bin/env python
+
 from pathlib import Path
 import requests
 
-from sickle import Sickle
 from tqdm import tqdm
 from geobench import config
 from tqdm.contrib.concurrent import thread_map
 import zipfile
 import hashlib
-
 
 BENCHMARK_MAP = {
     "classification": [
@@ -19,26 +19,97 @@ BENCHMARK_MAP = {
         "m-so2sat",
     ],
     "segmentation": [
-        # "m-chesapeake-lancover",
-        # "m-NeonTree",
+        "m-chesapeake-lancover",
+        "m-NeonTree",
         "m-nz-cattle",
-        # "m-pv4ger-seg",
-        # "m-cashew-plantation",
-        # "m-SA-crop-type",
+        "m-pv4ger-seg",
+        "m-cashew-plantation",
+        "m-SA-crop-type",
     ],
 }
 
+IDENTIFIERS = {
+    "m-NeonTree": "8008779",
+    "m-pv4ger-seg": "8008791",
+    "m-cashew-plantation": "8008799",
+    "m-SA-crop-type": "8008824",
+    "m-forestnet": "8008716",
+    "m-bigearthnet": "8008684",
+    "m-so2sat": "8008680",
+    "m-brick-kiln": "8008702",
+    "m-nz-cattle": "8008787",
+    "m-eurosat": "8008737",
+    "m-chesapeake-lancover": "8008757",
+    "m-pv4ger": "8008747",
+}
 
-def md5_checksum(file_path):
-    md5_hash = hashlib.md5()
-    with open(file_path, "rb") as file:
-        # Read and update hash in chunks of 4K
-        for byte_block in iter(lambda: file.read(4096), b""):
-            md5_hash.update(byte_block)
-    return md5_hash.hexdigest()
+
+def download_zenodo_file(file_url: str, output_path: Path, checksum: str = None, n_retry=2):
+    """Download a file from Zenodo and check the checksum. If the file already exists and the checksum is correct, skip the download."""
+    output_path = Path(output_path)
+    for attempt in range(n_retry):
+        try:
+            if file_exists_and_valid(output_path, checksum):
+                tqdm.write(f"File {output_path} already exists and checksum is good.")
+                return
+
+            download_file(file_url, output_path, checksum)
+            tqdm.write(f"Downloaded file: {file_url}")
+            return
+        except Exception as error:
+            tqdm.write(f"Attempt {attempt + 1} failed for file: {file_url}. Error: {error}")
+            if attempt + 1 == n_retry:
+                raise error
+
+
+def file_exists_and_valid(output_path: Path, checksum: str):
+    """Check if the file exists and the checksum is correct."""
+    if output_path.exists() and checksum:
+        return file_checksum(output_path) == checksum
+    return False
+
+
+def file_checksum(output_path: Path):
+    """Compute the MD5 checksum of a file."""
+    with output_path.open("rb") as file:
+        file_hash = hashlib.md5()
+        while chunk := file.read(8192):
+            file_hash.update(chunk)
+        return f"md5:{file_hash.hexdigest()}"
+
+
+def get_zenodo_record_by_url(url_or_identifier):
+    """Get the record data from a Zenodo URL."""
+    identifier = url_or_identifier.split("/")[-1]
+    record = requests.get(f"https://zenodo.org/api/records/{identifier}")
+    record.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+    return record.json()
+
+
+def download_file(file_url, output_path, checksum=None):
+    """Download a file from a URL and check the checksum."""
+    response = requests.get(file_url, stream=True)
+    response.raise_for_status()
+    total_size = int(response.headers.get("content-length", 0))
+    descr = "/".join(output_path.parts[-3:])
+    progress_bar = tqdm(total=total_size, unit="B", unit_scale=True, desc=descr, leave=False)
+
+    with output_path.open("wb") as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+            progress_bar.update(len(chunk))
+
+    progress_bar.close()
+
+    if checksum and file_checksum(output_path) != checksum:
+        raise ValueError(f"Checksum mismatch for file: {file_url}")
 
 
 def _extract_flat_zip(zip_file):
+    """Extract a zip file to the same directory.
+
+    This is temporary due to a bug in the upload
+    """
     with zipfile.ZipFile(zip_file) as zip:
         for zip_info in zip.infolist():
             if zip_info.is_dir():
@@ -47,102 +118,55 @@ def _extract_flat_zip(zip_file):
             zip.extract(zip_info, Path(zip_file).parent)
 
 
-def download_zenodo_file(file_url, output_path):
-    response = requests.get(file_url, stream=True)
-    if response.status_code == 200:
-        total_size = int(response.headers.get("content-length", 0))
+def download_dataset(files: list, dataset_dir: str):
+    """Download a dataset from Zenodo.
 
-        # take the last 3 elements of the output path
-        descr = "/".join(output_path.parts[-3:])
+    Parameters:
+        files (list): A list of files to download. Each file is a dict with keys "key", "links", and "checksum".
+        dataset_dir (str): The directory to download the dataset to.
 
-        progress_bar = tqdm(total=total_size, unit="B", unit_scale=True, desc=descr, leave=False)
-        with open(output_path, "wb") as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                file.write(chunk)
-                progress_bar.update(len(chunk))
-        progress_bar.close()
-
-        print(f"Downloaded file: {file_url}")
-    else:
-        print(f"Failed to download file: {file_url}. Status code: {response.status_code}")
-
-
-# def download_zenodo_record(record_data, output_dir):
-#     """
-#     Download the files from a Zenodo record
-
-#     Parameters
-#     ----------
-#     record_data : dict
-#         Dictionary containing the record data (retrieved from the Zenodo API)
-#     output_dir : str
-#         Output directory to save the files
-
-#     Returns
-#     -------
-#     None
-
-#     """
-#     # Extract file information
-#     files = record_data["files"]
-
-#     # Download each file
-#     for file in files:
-#         file_url = file["links"]["self"]
-#         file_name = file["key"]
-#         output_path = f"{output_dir}/{file_name}"
-
-#         # Download the file
-#         response = requests.get(file_url, stream=True)
-#         if response.status_code == 200:
-#             total_size = int(response.headers.get("content-length", 0))
-
-#             progress_bar = tqdm(total=total_size, unit="B", unit_scale=True, desc=file_name, leave=True)
-#             with open(output_path, "wb") as file:
-#                 for chunk in response.iter_content(chunk_size=8192):
-#                     file.write(chunk)
-#                     progress_bar.update(len(chunk))
-#             print(f"Downloaded file: {file_name}")
-#         else:
-#             print(f"Failed to download file: {file_name}. Status code: {response.status_code}")
-
-
-def download_dataset(files, dataset_dir):
+    """
     dataset_dir = Path(dataset_dir)
     dataset_dir.mkdir(parents=True, exist_ok=True)
+
+    done_path = dataset_dir / "done.txt"
+    if done_path.exists():
+        tqdm.write(f"Dataset {dataset_dir} already downloaded.")
+        return
 
     for file in files:
         output_path = dataset_dir / file["key"]
         url = file["links"]["self"]
-        download_zenodo_file(url, output_path=output_path)
-
-        md5 = md5_checksum(file_path=output_path)
-        if md5 != file["checksum"]:
-            raise Exception(f"MD5 checksum failed for file: {output_path}")
+        download_zenodo_file(url, output_path=output_path, checksum=file["checksum"])
 
         if output_path.suffix == ".zip":
             _extract_flat_zip(output_path)
             output_path.unlink()
 
-    (dataset_dir / "done.txt").touch()
+    done_path.touch()
 
 
 def download_benchmark(
-    version="v0.9.0", benchmark_namse=("classification", "segmentation"), geobench_dir=None
+    version="v0.9.0", benchmark_names=("classification", "segmentation"), geobench_dir=None
 ):
+    """Download geobench from Zenodo.
 
+    Parameters:
+        version (str): The version of the benchmark to download. Version 1.0.0 coming soon.
+        benchmark_names (tuple): The benchmarks to download. Can be "classification" or "segmentation".
+        geobench_dir (str): The directory to download the benchmark to. If None, use GEO_BENCH_DIR.
+    """
     if geobench_dir is None:
         geobench_dir = config.GEO_BENCH_DIR
     geobench_dir = Path(geobench_dir)
 
-    records = get_zenodo_records(version=version)
     all_files = []
 
-    for benchmark_name in benchmark_namse:
-        print(f"Downloading geobench: {benchmark_name} {version} to {geobench_dir}")
+    for benchmark_name in benchmark_names:
+        print(f"Collecting records for geobench: {benchmark_name} {version} to {geobench_dir}")
 
         for dataset_name in BENCHMARK_MAP[benchmark_name]:
-            record = records[dataset_name]
+            record = get_zenodo_record_by_url(IDENTIFIERS[dataset_name])
             dataset_dir = geobench_dir / f"{benchmark_name}_{version}" / dataset_name
             all_files.append((record["files"], dataset_dir))
 
@@ -150,26 +174,22 @@ def download_benchmark(
 
 
 def get_zenodo_records(version="v0.9.0", community="geo-bench"):
+    """Get all records from a Zenodo community. This is used to get the identifiers of all datasets."""
+
     # Browse all records in the geo-bench community
+    from sickle import Sickle
 
     sickle = Sickle("https://zenodo.org/oai2d")
 
     records = {}
     for record in sickle.ListRecords(metadataPrefix="oai_dc", set=f"user-{community}"):
-
         # Extract the record identifier
         identifier = [
             id.replace("https://zenodo.org/record/", "")
             for id in record.metadata["identifier"]
             if id.startswith("https://zenodo.org/record/")
         ][0]
-
-        # Fetch the record data
-        r = requests.get(f"https://zenodo.org/api/records/{identifier}")
-        if r.status_code != 200:
-            print("Error: Failed to fetch record:", identifier)
-            exit()
-        record = r.json()
+        record = get_zenodo_record_by_url(identifier)
 
         # Keep only records that have the correct version
         if version is not None:
@@ -181,10 +201,11 @@ def get_zenodo_records(version="v0.9.0", community="geo-bench"):
 
         records[dataset_name] = record
 
+    print({dataset_name: record["conceptrecid"] for dataset_name, record in records.items()})
+
     return records
 
 
 if __name__ == "__main__":
-
-    # record = get_zenodo_records()
-    download_benchmark(geobench_dir=Path("~/geobench_test").expanduser())
+    download_benchmark()
+    # download_benchmark(geobench_dir=Path("~/geobench_test").expanduser())
