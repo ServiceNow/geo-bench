4da950a247b39edd703ec1d1da8dcf2bcad154e4
recursix <alex.lacoste.shmu@gmail.com>
2023-08-21 14:19:36 +0000

    for backward compatibility

--- geobench/__init__.py
+++ geobench/__init__.py
@@ -5,3 +5,21 @@ __version__ = "0.0.1"
 from geobench.dataset import *
 from geobench.label import *
 from geobench.task import *
+
+
+# for backward compatibility of pickled objects
+import geobench.task as task
+import geobench.dataset as dataset
+import geobench.label as label
+import geobench
+
+import sys
+
+sys.modules["geobench.io.task"] = task
+sys.modules["geobench.io.dataset"] = dataset
+sys.modules["geobench.io.label"] = label
+sys.modules["geobench.io"] = geobench
+sys.modules["ccb.io.task"] = task
+sys.modules["ccb.io.dataset"] = dataset
+sys.modules["ccb.io.label"] = label
+sys.modules["ccb.io"] = geobench
