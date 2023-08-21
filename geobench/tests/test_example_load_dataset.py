be92bac0165aeaa9c34e3fcda5ab0b75f375db85
recursix <alex.lacoste.shmu@gmail.com>
2023-08-21 14:19:51 +0000

    test that the main example works

---
+++ geobench/tests/test_example_load_dataset.py
@@ -0,0 +1,19 @@
+import io
+import contextlib
+
+
+def test_load_dataset():
+    # just importing is enough to run it
+
+    captured_output = io.StringIO()
+    with contextlib.redirect_stdout(captured_output):
+        from geobench import example_load_datasets
+
+    output = captured_output.getvalue()
+
+    for word in ["Task", "Sample", "band", "eurosat", "pv4ger"]:
+        assert word in output, f"word {word} not found in output"
+
+
+if __name__ == "__main__":
+    test_load_dataset()
