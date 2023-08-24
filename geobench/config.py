cf3a62de706724ad55b7e6228c5ac089ecc66e3e
recursix <alex.lacoste.shmu@gmail.com>
2023-08-24 01:07:44 +0000

    jupyter doesn't always load env variables, try to gracefully handle it

--- geobench/config.py
+++ geobench/config.py
@@ -1,11 +1,11 @@
 import os
 from pathlib import Path
 
-_GEO_BENCH_DIR = os.environ.get("GEO_BENCH_DIR", None)
+_GEO_BENCH_DIR_ENV = os.environ.get("GEO_BENCH_DIR", None)
 
-if _GEO_BENCH_DIR is None:
+if _GEO_BENCH_DIR_ENV is None:
     GEO_BENCH_DIR = Path("~").expanduser() / "dataset" / "geobench"
 else:
-    GEO_BENCH_DIR = Path(_GEO_BENCH_DIR)
+    GEO_BENCH_DIR = Path(_GEO_BENCH_DIR_ENV)
 
 GEO_BENCH_DIR.mkdir(exist_ok=True, parents=True)
