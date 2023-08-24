7d520871f3d292fbf4a7fb4c3a15986a655904f0
recursix <alex.lacoste.shmu@gmail.com>
2023-08-24 01:08:12 +0000

    load all datasets to increase chances of spotting errors

--- geobench/example_load_datasets.py
+++ geobench/example_load_datasets.py
@@ -3,14 +3,18 @@
 
 import geobench
 
-for task in geobench.task_iterator(benchmark_name="classification_v0.9.0"):
-    print(f"Task {task.dataset_name}:\n  {task}\n")
+print(geobench.GEO_BENCH_DIR)
 
-    dataset = task.get_dataset(split="train")
-    sample = dataset[0]
+for benchmark_name in ("classification_v0.9.1", "segmentation_v0.9.1"):
+    print(f"Benchmark {benchmark_name}:\n")
+    for task in geobench.task_iterator(benchmark_name=benchmark_name):
+        print(f"Task {task.dataset_name}:\n  {task}\n")
 
-    print(f"Sample 0 named: {sample.sample_name}")
-    for band in sample.bands:
-        print(f"  {band.band_info.name}: {band.data.shape}")
+        dataset = task.get_dataset(split="train")
+        sample = dataset[0]
 
-    print("========================================\n")
+        print(f"Sample 0 named: {sample.sample_name}")
+        for band in sample.bands:
+            print(f"  {band.band_info.name}: {band.data.shape}")
+
+        print("========================================\n")
